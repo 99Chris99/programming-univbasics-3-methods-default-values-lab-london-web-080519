a25e9aa14b7ef809adbd0cdc750c7102d2c683e5
Chris Hodge <chrishodge87@gmail.com>
2019-07-10 17:01:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 # Your code here
 def meal_choice (veg1, veg2, protien = "meat")
-  line_one == puts "What a nutritious meal!"
-  line_tow == puts "A plate of #{protien} with #{veg1} and #{veg2}."
+  line_one = puts "What a nutritious meal!"
+  line_tow = puts "A plate of #{protien} with #{veg1} and #{veg2}."
 return line_one
 return line_two
 end
